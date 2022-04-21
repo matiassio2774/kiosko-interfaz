acfb2175b214c19b397592cc4bcd1ff99c171482
Matías Sio <94750147+matiassio2774@users.noreply.github.com>
2022-04-21 12:58:04 -0300

    Add files via upload

---
+++ CARAMELOS/Form1.Designer.vb
@@ -0,0 +1,350 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.BtnInsertar = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtid = New System.Windows.Forms.TextBox()
+        Me.txtname = New System.Windows.Forms.TextBox()
+        Me.txtCantidad = New System.Windows.Forms.TextBox()
+        Me.txtPrecio = New System.Windows.Forms.TextBox()
+        Me.btnbuscar = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.IdDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.NombreDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.CantidadDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.PrecioDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.GolosinasBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.KioscoDataSet = New CARAMELOS.kioscoDataSet()
+        Me.GolosinasTableAdapter = New CARAMELOS.kioscoDataSetTableAdapters.golosinasTableAdapter()
+        Me.BtnModificar = New System.Windows.Forms.Button()
+        Me.BtnEliminar = New System.Windows.Forms.Button()
+        Me.BtnModPrecio = New System.Windows.Forms.Button()
+        Me.TxtModPrecio = New System.Windows.Forms.TextBox()
+        Me.RBBajar = New System.Windows.Forms.RadioButton()
+        Me.RBSubir = New System.Windows.Forms.RadioButton()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TxtId2 = New System.Windows.Forms.TextBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GolosinasBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.KioscoDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'BtnInsertar
+        '
+        Me.BtnInsertar.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnInsertar.Location = New System.Drawing.Point(37, 245)
+        Me.BtnInsertar.Name = "BtnInsertar"
+        Me.BtnInsertar.Size = New System.Drawing.Size(91, 39)
+        Me.BtnInsertar.TabIndex = 0
+        Me.BtnInsertar.Text = "Insertar"
+        Me.BtnInsertar.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(65, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(19, 16)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "ID"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(51, 78)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 16)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Nombre"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(51, 129)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(62, 16)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Cantidad"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(56, 184)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(41, 16)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Precio"
+        '
+        'txtid
+        '
+        Me.txtid.Location = New System.Drawing.Point(28, 46)
+        Me.txtid.Name = "txtid"
+        Me.txtid.Size = New System.Drawing.Size(100, 20)
+        Me.txtid.TabIndex = 5
+        '
+        'txtname
+        '
+        Me.txtname.Location = New System.Drawing.Point(28, 97)
+        Me.txtname.Name = "txtname"
+        Me.txtname.Size = New System.Drawing.Size(100, 20)
+        Me.txtname.TabIndex = 6
+        '
+        'txtCantidad
+        '
+        Me.txtCantidad.Location = New System.Drawing.Point(28, 148)
+        Me.txtCantidad.Name = "txtCantidad"
+        Me.txtCantidad.Size = New System.Drawing.Size(100, 20)
+        Me.txtCantidad.TabIndex = 7
+        '
+        'txtPrecio
+        '
+        Me.txtPrecio.Location = New System.Drawing.Point(28, 200)
+        Me.txtPrecio.Name = "txtPrecio"
+        Me.txtPrecio.Size = New System.Drawing.Size(100, 20)
+        Me.txtPrecio.TabIndex = 8
+        '
+        'btnbuscar
+        '
+        Me.btnbuscar.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnbuscar.Location = New System.Drawing.Point(192, 245)
+        Me.btnbuscar.Name = "btnbuscar"
+        Me.btnbuscar.Size = New System.Drawing.Size(91, 39)
+        Me.btnbuscar.TabIndex = 9
+        Me.btnbuscar.Text = "Buscar"
+        Me.btnbuscar.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IdDataGridViewTextBoxColumn, Me.NombreDataGridViewTextBoxColumn, Me.CantidadDataGridViewTextBoxColumn, Me.PrecioDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataSource = Me.GolosinasBindingSource
+        Me.DataGridView1.Location = New System.Drawing.Point(161, 59)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(443, 138)
+        Me.DataGridView1.TabIndex = 10
+        '
+        'IdDataGridViewTextBoxColumn
+        '
+        Me.IdDataGridViewTextBoxColumn.DataPropertyName = "id"
+        Me.IdDataGridViewTextBoxColumn.HeaderText = "id"
+        Me.IdDataGridViewTextBoxColumn.Name = "IdDataGridViewTextBoxColumn"
+        '
+        'NombreDataGridViewTextBoxColumn
+        '
+        Me.NombreDataGridViewTextBoxColumn.DataPropertyName = "nombre"
+        Me.NombreDataGridViewTextBoxColumn.HeaderText = "nombre"
+        Me.NombreDataGridViewTextBoxColumn.Name = "NombreDataGridViewTextBoxColumn"
+        '
+        'CantidadDataGridViewTextBoxColumn
+        '
+        Me.CantidadDataGridViewTextBoxColumn.DataPropertyName = "cantidad"
+        Me.CantidadDataGridViewTextBoxColumn.HeaderText = "cantidad"
+        Me.CantidadDataGridViewTextBoxColumn.Name = "CantidadDataGridViewTextBoxColumn"
+        '
+        'PrecioDataGridViewTextBoxColumn
+        '
+        Me.PrecioDataGridViewTextBoxColumn.DataPropertyName = "precio"
+        Me.PrecioDataGridViewTextBoxColumn.HeaderText = "precio"
+        Me.PrecioDataGridViewTextBoxColumn.Name = "PrecioDataGridViewTextBoxColumn"
+        '
+        'GolosinasBindingSource
+        '
+        Me.GolosinasBindingSource.DataMember = "golosinas"
+        Me.GolosinasBindingSource.DataSource = Me.KioscoDataSet
+        '
+        'KioscoDataSet
+        '
+        Me.KioscoDataSet.DataSetName = "kioscoDataSet"
+        Me.KioscoDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'GolosinasTableAdapter
+        '
+        Me.GolosinasTableAdapter.ClearBeforeFill = True
+        '
+        'BtnModificar
+        '
+        Me.BtnModificar.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnModificar.Location = New System.Drawing.Point(362, 245)
+        Me.BtnModificar.Name = "BtnModificar"
+        Me.BtnModificar.Size = New System.Drawing.Size(92, 39)
+        Me.BtnModificar.TabIndex = 11
+        Me.BtnModificar.Text = "Modificar"
+        Me.BtnModificar.UseVisualStyleBackColor = True
+        '
+        'BtnEliminar
+        '
+        Me.BtnEliminar.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnEliminar.Location = New System.Drawing.Point(512, 245)
+        Me.BtnEliminar.Name = "BtnEliminar"
+        Me.BtnEliminar.Size = New System.Drawing.Size(92, 39)
+        Me.BtnEliminar.TabIndex = 12
+        Me.BtnEliminar.Text = "Eliminar"
+        Me.BtnEliminar.UseVisualStyleBackColor = True
+        '
+        'BtnModPrecio
+        '
+        Me.BtnModPrecio.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnModPrecio.Location = New System.Drawing.Point(632, 216)
+        Me.BtnModPrecio.Margin = New System.Windows.Forms.Padding(2)
+        Me.BtnModPrecio.Name = "BtnModPrecio"
+        Me.BtnModPrecio.Size = New System.Drawing.Size(137, 25)
+        Me.BtnModPrecio.TabIndex = 25
+        Me.BtnModPrecio.Text = "Modificar Precio" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.BtnModPrecio.UseVisualStyleBackColor = True
+        '
+        'TxtModPrecio
+        '
+        Me.TxtModPrecio.Location = New System.Drawing.Point(632, 184)
+        Me.TxtModPrecio.Margin = New System.Windows.Forms.Padding(2)
+        Me.TxtModPrecio.Name = "TxtModPrecio"
+        Me.TxtModPrecio.Size = New System.Drawing.Size(137, 20)
+        Me.TxtModPrecio.TabIndex = 24
+        '
+        'RBBajar
+        '
+        Me.RBBajar.AutoSize = True
+        Me.RBBajar.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RBBajar.Location = New System.Drawing.Point(716, 97)
+        Me.RBBajar.Margin = New System.Windows.Forms.Padding(2)
+        Me.RBBajar.Name = "RBBajar"
+        Me.RBBajar.Size = New System.Drawing.Size(53, 20)
+        Me.RBBajar.TabIndex = 23
+        Me.RBBajar.Text = "Bajar"
+        Me.RBBajar.UseVisualStyleBackColor = True
+        '
+        'RBSubir
+        '
+        Me.RBSubir.AutoSize = True
+        Me.RBSubir.Checked = True
+        Me.RBSubir.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RBSubir.Location = New System.Drawing.Point(632, 97)
+        Me.RBSubir.Margin = New System.Windows.Forms.Padding(2)
+        Me.RBSubir.Name = "RBSubir"
+        Me.RBSubir.Size = New System.Drawing.Size(52, 20)
+        Me.RBSubir.TabIndex = 22
+        Me.RBSubir.TabStop = True
+        Me.RBSubir.Text = "Subir"
+        Me.RBSubir.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(629, 166)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(80, 16)
+        Me.Label5.TabIndex = 26
+        Me.Label5.Text = "+/- Cantidad"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(629, 126)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(19, 16)
+        Me.Label6.TabIndex = 28
+        Me.Label6.Text = "ID"
+        '
+        'TxtId2
+        '
+        Me.TxtId2.Location = New System.Drawing.Point(632, 144)
+        Me.TxtId2.Margin = New System.Windows.Forms.Padding(2)
+        Me.TxtId2.Name = "TxtId2"
+        Me.TxtId2.Size = New System.Drawing.Size(137, 20)
+        Me.TxtId2.TabIndex = 27
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(819, 326)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TxtId2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.BtnModPrecio)
+        Me.Controls.Add(Me.TxtModPrecio)
+        Me.Controls.Add(Me.RBBajar)
+        Me.Controls.Add(Me.RBSubir)
+        Me.Controls.Add(Me.BtnEliminar)
+        Me.Controls.Add(Me.BtnModificar)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btnbuscar)
+        Me.Controls.Add(Me.txtPrecio)
+        Me.Controls.Add(Me.txtCantidad)
+        Me.Controls.Add(Me.txtname)
+        Me.Controls.Add(Me.txtid)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtnInsertar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GolosinasBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.KioscoDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BtnInsertar As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtid As TextBox
+    Friend WithEvents txtname As TextBox
+    Friend WithEvents txtCantidad As TextBox
+    Friend WithEvents txtPrecio As TextBox
+    Friend WithEvents btnbuscar As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents KioscoDataSet As kioscoDataSet
+    Friend WithEvents GolosinasBindingSource As BindingSource
+    Friend WithEvents GolosinasTableAdapter As kioscoDataSetTableAdapters.golosinasTableAdapter
+    Friend WithEvents IdDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents NombreDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents CantidadDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents PrecioDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents BtnModificar As Button
+    Friend WithEvents BtnEliminar As Button
+    Friend WithEvents BtnModPrecio As Button
+    Friend WithEvents TxtModPrecio As TextBox
+    Friend WithEvents RBBajar As RadioButton
+    Friend WithEvents RBSubir As RadioButton
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TxtId2 As TextBox
+End Class
---
+++ CARAMELOS/Form1.vb
@@ -0,0 +1,134 @@
+﻿Public Class Form1
+
+    Private Sub actualizarPantalla()
+        Me.GolosinasTableAdapter.Fill(Me.KioscoDataSet.golosinas)
+    End Sub
+
+    Private Sub btnInsertar_Click(sender As Object, e As EventArgs) Handles BtnInsertar.Click
+        Dim sql As String
+        Dim conex As New SQLConnect
+        Try
+            'Consulta SQL para insertar registros en la tabla golosinas
+            sql = "use kiosco; insert into golosinas values('" + txtid.Text + "', '" + txtname.Text + "', '" + txtCantidad.Text + "', '" + txtPrecio.Text + "');"
+            conex.EjecutarSQL(sql)
+            conex.Dispose()
+            MsgBox("Registro insertado correctamente")
+
+        Catch ex As Exception
+            MsgBox("Error al insertar registro")
+        End Try
+    End Sub
+
+    Private Sub btnbuscar_Click(sender As Object, e As EventArgs) Handles btnbuscar.Click
+        Me.GolosinasTableAdapter.Fill(Me.KioscoDataSet.golosinas)
+        Dim sql As String
+        Dim conex As New SQLConnect
+        'Consulta SQL para buscar una golosina en la tabla
+        Try
+            sql = "USE kiosco; SELECT * FROM kiosco.golosinas WHERE id LIKE('" + txtid.Text + "') OR nombre LIKE('" + txtname.Text + "');"
+            conex.EjecutarSQL(sql)
+
+            conex.DataAdapter = New MySql.Data.MySqlClient.MySqlDataAdapter(conex.miComando)
+            conex.DataAdapter.Fill(conex.DataSet)
+            DataGridView1.DataSource = conex.DataSet.Tables(0).DefaultView
+
+            conex.Dispose()
+            MsgBox("Consulta ejecutada correctamente.")
+            actualizarPantalla()
+        Catch ex As Exception
+            MsgBox("Error al realizar la busqueda.")
+        End Try
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim sql As String
+        Dim conex As New SQLConnect
+        Try
+            'Consulta SQL para tirar select a todos los registros de la tabla golosinas
+            sql = "USE kiosco; SELECT * FROM kiosco.golosinas;"
+            conex.EjecutarSQL(sql)
+            conex.Dispose()
+            MsgBox("Acceso permitido")
+
+        Catch ex As Exception
+            'Crea el schema kiosko y la tabla con sus columnas
+            sql = " Create schema kiosco; use kiosco; Create table golosinas (id tinyint Primary Key, nombre varchar(20), cantidad int, precio float);"
+            conex.EjecutarSQL(sql)
+            conex.Dispose()
+            MsgBox("Tabla creada correctamente!")
+        End Try
+        Me.GolosinasTableAdapter.Fill(Me.KioscoDataSet.golosinas)
+        Me.DataGridView1.ReadOnly = True ' Se hace el DGV para solo lectura
+    End Sub
+
+    Private Sub BtnModificar_Click(sender As Object, e As EventArgs) Handles BtnModificar.Click
+        Dim sql As String
+        Dim conex As New SQLConnect
+
+
+        Try
+            'Consulta para updatear las golosinas
+            sql = "USE kiosco; UPDATE golosinas SET nombre = '" + txtname.Text + "', cantidad= '" + txtCantidad.Text + "' ,precio='  " + txtPrecio.Text + "' WHERE id=(' " + txtid.Text + "');"
+
+            conex.EjecutarSQL(sql)
+            conex.Dispose()
+
+            MsgBox("Registro modificado correctamente.")
+            actualizarPantalla()
+
+        Catch ex As Exception
+
+            MsgBox("Error al realizar la mofidicacion")
+
+        End Try
+    End Sub
+
+    Private Sub BtnEliminar_Click(sender As Object, e As EventArgs) Handles BtnEliminar.Click
+        Dim sql As String
+        Dim conex As New SQLConnect
+
+
+        Try
+            'Consulta para dropear un registro.
+            sql = "USE kiosco; DELETE FROM golosinas WHERE id=(' " + txtid.Text + "');"
+
+            conex.EjecutarSQL(sql)
+            conex.Dispose()
+
+            MsgBox("Registro eliminado correctamente.")
+
+        Catch ex As Exception
+
+            MsgBox("Error al eliminar el registro.")
+
+        End Try
+    End Sub
+
+    Private Sub BtnModPrecio_Click(sender As Object, e As EventArgs) Handles BtnModPrecio.Click
+        Dim sql As String
+        Dim conex As New SQLConnect
+
+
+        If RBSubir.Checked = True Then
+            sql = "USE kiosco; UPDATE golosinas SET precio = precio + " + TxtModPrecio.Text + " WHERE id = " + TxtId2.Text + ";"
+            conex.EjecutarSQL(Sql)
+            conex.Dispose()
+
+            MsgBox("Precio modificado correctamente.")
+            actualizarPantalla()
+
+        ElseIf RBBajar.Checked Then
+            sql = "USE kiosco; UPDATE golosinas SET precio = precio - " + TxtModPrecio.Text + " WHERE id = " + TxtId2.Text + ";"
+
+            conex.EjecutarSQL(Sql)
+            conex.Dispose()
+
+            MsgBox("Precio modificado correctamente.")
+            actualizarPantalla()
+
+        End If
+    End Sub
+End Class
